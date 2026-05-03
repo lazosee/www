8f6f9066d02b090cd98f6610398b4400dafac781
lazosee <oseelazaro@gmail.com>
2026-05-03 22:33:39 +0300

    Style footer component

--- src/components/global/data.ts
+++ src/components/global/data.ts
@@ -4,7 +4,6 @@ export const nav_links = {
 	Home: '/',
 	Blueprints: '/blueprints',
 	Sketches: '/sketches',
-	About: '/about',
 	Contact: '/contact',
 } satisfies Link
 
---
+++ src/utils/functions.ts
@@ -0,0 +1,3 @@
+export function sortByLength(a: string, b: string) {
+	return a.length - b.length
+}
